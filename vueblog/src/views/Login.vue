7cec1092146d93ab993d5d3faf4276ff8d6c40a7
zhouwei <1244620067@qq.com>
2021-01-27 19:59:33 +0800

    login.vue

---
+++ vueblog/src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <el-form label-width="80px">
+            <el-form-item label="账号">
+                <el-input v-model="username"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+                <el-input v-model="password"></el-input>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+        data(){
+            return{
+                username:'',
+                password:'',
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
